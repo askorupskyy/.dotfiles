6dbd71da7d5b5ae07a7adc22d81f7cbbeb82d6e2
rcbxd <50280805+rcbxd@users.noreply.github.com>
2022-12-10 22:52:22 -0600

    ability to view hidden dotfiles in telescope finder

--- .config/nvim/lua/config/telescope/finders.lua
+++ .config/nvim/lua/config/telescope/finders.lua
@@ -14,6 +14,7 @@ finders.find_dotfiles = function()
 		cwd = "~/.dotfiles",
 		prompt_title = "~ dotfiles ~",
 		shorten_path = false,
+    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
 	}
 	require("telescope.builtin").find_files(opts)
 end
