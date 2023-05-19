eb819d05e5e7d14e6a02e042e98fa4c83beb08c0
rcbxd <50280805+rcbxd@users.noreply.github.com>
2023-05-19 00:14:25 -0500

    (fish) git unstage script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,19 @@ end
 set pure_enable_single_line_prompt true
 set pure_show_system_time true
 set pure_show_system_time_pos 'right'
+
 # set --universal nvm_default_version latest
 set --universal nvm_default_packages yarn np
+
+# git aliases
+function git
+    if test "$argv[1]" = "unstage"
+        command git restore --staged $argv[2..-1]
+    else
+        command git $argv
+    end
+end
+
+
+complete -c git -f -a "unstage" -d "Unstage changes"
+complete -c git -n '__fish_seen_subcommand_from unstage' -a '(__fish_complete_path)'
