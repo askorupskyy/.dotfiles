80f12db691496ca4071e4b4b03880f905a9d1355
askorupskyy <50280805+rcbxd@users.noreply.github.com>
2025-10-22 23:49:49 -0500

    create `gb` function for finder

--- .config/fish/pkgs/finder.fish
+++ .config/fish/pkgs/finder.fish
@@ -1,4 +1,4 @@
-# my own implementation of well-known ff function
+# my own implementation of well-known ff, fg, and gb functions
 # uses rg to list files, fzf to select one and bat to preview
 
 function ff --description "Search files in current dir"
@@ -7,6 +7,8 @@ function ff --description "Search files in current dir"
   if test -n "$file"
     $EDITOR $file
   end
+
+  print "No file selected"
 end
 
 
@@ -16,4 +18,16 @@ function fg --description "Search files based on contents"
   if test -n "$file"
     $EDITOR $file
   end
+
+  print "No file selected"
+end
+
+function gb --description "Search git branches"
+  set -l branch (git branch --all --color=never | sed 's/^[* ]*//' | fzf --preview "git log -n 10 --pretty=format:'%C(yellow)%h %C(cyan)%ad %C(green)%an%Creset %s' {1}" --border --prompt="Git branches > " "$_fzf_layout_window")
+
+  if test -n "$branch"
+    git checkout (string trim (string replace 'remotes/origin/' '' $branch))
+  end
+
+  print "No branch selected"
 end
